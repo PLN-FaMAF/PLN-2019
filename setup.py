6d134d2faacaf3db4a7914650f82bd20ce071940
Franco M. Luque <francolq@famaf.unc.edu.ar>
2019-03-14 16:23:54 -0300

    Forgot to add setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pln-2019',
+    # Read the following pages for advice on version numbering:
+    # http://pythonhosted.org/setuptools/setuptools.html
+    # http://docs.python.org/distutils/setupscript.html
+    version='0.1',
+    author='Franco M. Luque',
+    author_email='francolq@famaf.unc.edu.ar',
+    url='http://www.cs.famaf.unc.edu.ar/~francolq/',
+    packages=find_packages(),
+)
